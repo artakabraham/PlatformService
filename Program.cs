83e0cff2fbc56e7bd8704cda426baae8730b8bbd
Artak <abrahamyan@outlook.com>
2022-04-25 21:40:49 +0400

    Data Layer - Data Transfer Objects

--- Program.cs
+++ Program.cs
@@ -10,6 +10,7 @@ builder.Services.AddDbContext<AppDbContext>(opt => opt.UseInMemoryDatabase("InMe
 builder.Services.AddScoped<IPlatformRepo, PlatformRepo>();
 
 builder.Services.AddControllers();
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
