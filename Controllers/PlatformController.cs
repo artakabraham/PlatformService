af9b06b125cacb8510c5229395e0dfd1794c5468
Artak <abrahamyan@outlook.com>
2022-04-26 11:53:40 +0400

    Controller and Actions-1

---
+++ Controllers/PlatformController.cs
@@ -0,0 +1,29 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using PlatformService.Data;
+using PlatformService.Dtos;
+
+namespace PlatformService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PlatformController : ControllerBase
+    {
+        private readonly IPlatformRepo _repository;
+        private IMapper _mapper;
+
+        public PlatformController(IPlatformRepo repository, IMapper mapper)
+        {
+            _repository = repository;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<PlatformReadDto>> GetPlatforms()
+        {
+            Console.WriteLine("--> Getting Platforms...");
+            var platformItem =  _repository.GetAllPLatforms();
+            return Ok(_mapper.Map<IEnumerable<PlatformReadDto>>(platformItem));
+        }
+    }
+}
