1ff145ff1e1dff4d3b4e8a809b33b213faac7cef
Artak <abrahamyan@outlook.com>
2022-05-15 23:45:56 +0400

    Adding a HTTP Client

---
+++ SyncDataServices/Http/HttpCommandDataClient.cs
@@ -0,0 +1,34 @@
+﻿using PlatformService.Dtos;
+using System.Text;
+using System.Text.Json;
+
+namespace PlatformService.SyncDataServices.Http
+{
+    public class HttpCommandDataClient : ICommandDataClient
+    {
+        private readonly HttpClient _httpClient;
+        private IConfiguration _configuration;
+
+        public HttpCommandDataClient(HttpClient httpClient, IConfiguration configuration)
+        {
+            _httpClient = httpClient;
+            _configuration = configuration;
+        }
+
+        public async Task SendPlatformToCommand(PlatformReadDto plat)
+        {
+            var httpContent = new StringContent(JsonSerializer.Serialize(plat), Encoding.UTF8, "application/json");
+
+            var response = await _httpClient.PostAsync(_configuration["CommandService"], httpContent);
+
+            if (response.IsSuccessStatusCode)
+            {
+                Console.WriteLine("--> Sync POST CommandService was OK");
+            }
+            else
+            {
+                Console.WriteLine("--> Sync POST CommandService was not OK");
+            }
+        }
+    }
+}
